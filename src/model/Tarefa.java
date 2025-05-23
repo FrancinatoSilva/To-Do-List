132505e3f4aca69ea29daf91157f0968610db87e
Francis Lean <irenofrancinato@gmail.com>
2025-05-23 20:56:19 -0300

    ImNomenclatura de pacotes e implementação da class Tarefa

---
+++ src/model/Tarefa.java
@@ -0,0 +1,63 @@
+package model;
+
+public class Tarefa {
+
+    private int id;
+    private String descricao;
+    private Prioridade prioridade;
+    private boolean concluida;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(int id, String descricao, Prioridade prioridade, boolean concluida) {
+        this.id = id;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.concluida = concluida;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Prioridade getPrioridade() {
+        return this.prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public boolean getConcluida() {
+        return this.concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public enum Prioridade {
+        BAIXA, MEDIA, ALTA;
+    }
+
+    public void exibirTarefa() {
+        if (concluida) {
+            System.out.println("[X] " + getDescricao() + " (" + getPrioridade() + ")");
+        } else {
+            System.out.println("[ ] " + getDescricao() + " (" + getPrioridade() + ")");
+        }
+    }
+}
