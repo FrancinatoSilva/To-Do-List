44b481ec00cf3f5f217408351f7bee3a55470cbb
Francis <irenofrancinato@gmail.com>
2025-06-07 16:49:53 -0300

    Correção de bug na primeira entrada Antes a primeira entrada do usuário estava passivel a quebra da aplicação, solucionei isso com tratamento de erros

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -12,52 +12,66 @@ public class ConsoleView {
         GerenciadorDeTarefas gerenciadorDeTarefas = new GerenciadorDeTarefas();
         Tarefa tarefa = new Tarefa();
 
-        int opcaoDesejada;
+        int opcaoDesejada = 0;
         do {
             gerenciadorDeTarefas.exibirMenu();
-            System.out.print("Opção desejada:");
-            opcaoDesejada = sc.nextInt();
 
-            Tarefa.Prioridade prioridade = null;
-
-            switch (opcaoDesejada) {
-                case 1:
-                    sc.nextLine();
-                    System.out.print("Digite a descrição da tarefa: ");
-                    String descricao = sc.nextLine();
-                    System.out.print("Digite a prioridade (BAIXA, MEDIA, ALTA): ");
-                    String input = sc.nextLine().toUpperCase();
-
-                    try {
-                        prioridade = Tarefa.Prioridade.valueOf(input);
-                        System.out.println("Você escolheu: " + prioridade);
-                    } catch (IllegalArgumentException e) {
-                        System.out.println("Prioridade inválida. Tente novamente.");
+            while (true) {
+                try {
+                    System.out.print("Opção desejada:");
+                    if (sc.hasNextInt()) {
+                        opcaoDesejada = sc.nextInt();
+                        break;
+                    } else {
+                        System.out.println("INSIRA UM NÚMERO INTEIRO");
+                        sc.next(); // Limpa o scanner removendo o caractere inválido
                     }
-
-                    gerenciadorDeTarefas.adicionarTarefa(descricao, prioridade);
-                    break;
-                case 2:
-                    gerenciadorDeTarefas.listarTarefas();
-                    break;
-                case 3:
-                    gerenciadorDeTarefas.removerTarefa(sc);
-                    break;
-                case 4:
-                    gerenciadorDeTarefas.marcarConcluida(sc);
-                    break;
-                case 5:
-                    gerenciadorDeTarefas.editarTarefa(sc);
-                    break;
-                case 6:
-                    System.out.println("Encerrando execução...");
-                    return;
-                default:
-                    System.out.println("OPÇÃO INVÁLIDA!");
-
+                } catch (Exception e) {
+                    System.out.println("Ocorreu um erro. Insira um número inteiro.");
+                    sc.next();
+                }
             }
-            System.out.println("------------------------------------------------");
-        } while (opcaoDesejada != 6);
+
+                Tarefa.Prioridade prioridade = null;
+
+                switch (opcaoDesejada) {
+                    case 1:
+                        sc.nextLine();
+                        System.out.print("Digite a descrição da tarefa: ");
+                        String descricao = sc.nextLine();
+                        System.out.print("Digite a prioridade (BAIXA, MEDIA, ALTA): ");
+                        String input = sc.nextLine().toUpperCase();
+
+                        try {
+                            prioridade = Tarefa.Prioridade.valueOf(input);
+                            System.out.println("Você escolheu: " + prioridade);
+                        } catch (IllegalArgumentException e) {
+                            System.out.println("Prioridade inválida. Tente novamente.");
+                        }
+
+                        gerenciadorDeTarefas.adicionarTarefa(descricao, prioridade);
+                        break;
+                    case 2:
+                        gerenciadorDeTarefas.listarTarefas();
+                        break;
+                    case 3:
+                        gerenciadorDeTarefas.removerTarefa(sc);
+                        break;
+                    case 4:
+                        gerenciadorDeTarefas.marcarConcluida(sc);
+                        break;
+                    case 5:
+                        gerenciadorDeTarefas.editarTarefa(sc);
+                        break;
+                    case 6:
+                        System.out.println("Encerrando execução...");
+                        return;
+                    default:
+                        System.out.println("OPÇÃO INVÁLIDA!");
+
+                }
+                System.out.println("------------------------------------------------");
+        }while (opcaoDesejada != 6) ;
 
         sc.close();
     }
